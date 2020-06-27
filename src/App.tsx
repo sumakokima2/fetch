2ed88f3b40a1c1e67e43435142fac72310e04d84
原田真喜子 <makikoharada@haradamakiko-no-MacBook-Air-2.local>
2020-06-28 01:43:29 +0900

    url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [data,setData] = useState("");
   useEffect( ()=>{
     const loadData = async () =>{
-      const response = await fetch("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=27c093eb8a748dff&large_area=Z011&format=json.json");
+      const response = await fetch("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=27c093eb8a748dff&large_area=Z011&format=json");
       const thisdata = await response.json();
       console.log(thisdata);
       console.log(data);
