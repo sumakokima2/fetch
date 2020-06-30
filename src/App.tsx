9376a711cdcb76497caac53a6876962856fc2169
原田真喜子 <makikoharada@haradamakiko-no-MacBook-Air-2.local>
2020-06-30 11:00:28 +0900

    CORSカイケツシマセン

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,65 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import './recruit.js';
-//import data from './recruit.json';
+import axios from 'axios';
+//import './recruit.js';
+//import dataa from './recruit.json';
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 function App() {
   
-  const [data,setData] = useState("");
-  useEffect( ()=>{
+  const [data, setData] = useState({ hits: [] });
+  const [fdata, setFData] = useState({ hits: [] });
 
-    const loadData = async () =>{
-
-      const responsea:any =　fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=27c093eb8a748dff&large_area=Z011&format=json&count=2',{
-        mode: 'cors'
-      })
-      .then(response => {
-        return response.json();
-      })
-      .then(body => {
-        console.log(JSON.stringify(body));
-      });
-      console.log(responsea);
-
-      setData(responsea);
-      console.log(data);
+  useEffect(() => {
+    const axiosData = async () => {
+      const result = await axios(
+        'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=27c093eb8a748dff&large_area=Z011&format=json',
+        {
+          withCredentials: true
+        });
+       // axios.defaults.withCredentials = true; // global に設定してしまう場合
+      setData(result.data);
+      console.log(result);
     };
-    loadData();
-    },[data]);
-  //const aa:any =   data; 
-  //console.log(data);
-    //
+    
 
+    const fetchData=()=>{
+      fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=27c093eb8a748dff&large_area=Z011&format=json',
+      {method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'API-Key': '27c093eb8a748dff'
+        }
+       })
+      .then(function (response) {
+       // handle success
+        console.log(response)
+        setFData(response.json);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    } 
+    axiosData();
+    fetchData();
+    },[]);
+/* {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}*/       
 
   return (
     <div className="App">
-      
+       <ul>
+     
+    </ul>
     </div>
   );
 }
